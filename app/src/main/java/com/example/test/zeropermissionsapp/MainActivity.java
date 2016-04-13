61c2f4f7f074b443985ed5a4030f267e9e5543cf
Jesper <1jesper1@gmail.com>
2016-04-14 00:04:42 +0200

    Intent parameter to send value when app starts, disable back press, restart app when home button pressed

--- app/src/main/java/com/example/test/zeropermissionsapp/MainActivity.java
+++ app/src/main/java/com/example/test/zeropermissionsapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.test.zeropermissionsapp;
 
+import android.app.ActivityManager;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
@@ -9,10 +11,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.test.zeropermissionsapp.receivers.ScreenSleepReceiver;
 
@@ -27,7 +31,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Dangerous application");
         setSupportActionBar(toolbar);
-
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            Toast.makeText(this, extras.getString("toast"), Toast.LENGTH_LONG).show();
+        }
         IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
         BroadcastReceiver mReceiver = new ScreenSleepReceiver();
         registerReceiver(mReceiver, filter);
@@ -50,5 +57,20 @@ public class MainActivity extends AppCompatActivity {
                 da.sendIR();
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "Back press stay", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.putExtra("toast", "Hallo");
+        this.startActivity(intent);
     }
 }
